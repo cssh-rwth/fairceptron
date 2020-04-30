d1b6617e334487d40ec62cc6976a1ea0f87d75d9
Georg Ahnert <mail@geahnert.de>
2020-04-30 10:39:31 +0200

    Init rating bar

---
+++ components/ratingbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <ul>
+      <li v-for="ratingLevel in ratingLevels" :key="ratingLevel">
+        {{ ratingLevel }}
+      </li>
+    </ul>
+    <button type="submit">{{ confirmName }}</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'RatingBar',
+  data() {
+    return {
+      ratingLevels: [
+        'sehr unfair',
+        'etwas unfair',
+        'ausgeglichen',
+        'etwas fair',
+        'sehr fair'
+      ],
+      confirmName: 'Nächste Frage'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
