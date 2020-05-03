77ab7872caa288081899599e2c399c34b871d8c6
Georg Ahnert <mail@geahnert.de>
2020-05-03 22:25:31 +0200

    Improve wording in description

--- components/QuestionDescription.vue
+++ components/QuestionDescription.vue
@@ -4,15 +4,16 @@
   >
     Dieses Szenario besteht aus {{ personas.length }} Personen in
     {{ noOfGroups }} Gruppen:
-    <span v-for="n in noOfGroups" :key="n">
-      {{ n === noOfGroups ? 'und' : n > 1 ? ',' : '' }}
+    <span v-for="n in noOfGroups" :key="n"
+      >{{ n === noOfGroups ? ' und' : n > 1 ? ',' : '' }}
       <span :class="colors[n - 1]" class="font-bold">
-        {{ personasPerGroup[n - 1] }} {{ groupNames[n - 1] }}</span
+        {{ personasPerGroup[n - 1] }} {{ groupDescriptions[n - 1] }}</span
       ></span
     >.
     <br />
     <span v-if="questionType === 'selection'">
-      Es werden {{ noSelected }} Personen
+      <span v-if="noSelected > 1">Es werden {{ noSelected }} Personen</span>
+      <span v-else>Es wird eine Person</span>
       <span class="border-2 p-px px-1 border-green-500 rounded bg-green-100">
         ausgewählt</span
       >.
@@ -49,7 +50,18 @@ export default {
       'personasPerGroup',
       'noOfGroups',
       'noSelected'
-    ])
+    ]),
+    groupDescriptions() {
+      if (this.groupNames.length > 0) return this.groupNames
+      else {
+        const descriptions = []
+        for (let i = 0; i < this.noOfGroups; i++) {
+          const descr = this.personasPerGroup[i] === 1 ? 'Person' : 'Personen'
+          descriptions.push(descr + ' in Gruppe ' + (i + 1).toString())
+        }
+        return descriptions
+      }
+    }
   }
 }
 </script>
