8b513bb90e2e51011687e6009a8900adeb982eb6
Georg Ahnert <mail@geahnert.de>
2020-06-21 14:18:14 +0200

    Move DB credentials to .env

--- api/mongoose/service.js
+++ api/mongoose/service.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const options = {
   // geting rid off the depreciation errors
@@ -6,7 +7,7 @@ const options = {
   useUnifiedTopology: true
 }
 mongoose.connect(
-  'mongodb://mongoose:qW8vj27cHwRD8XtxyBLH@fairceptron.ahnert.dev:27017/fairceptronDB?authSource=fairceptronDB',
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@fairceptron.ahnert.dev:27017/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`,
   options
 )
 
