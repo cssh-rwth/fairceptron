fc2f7fe215f457984c53d38081f921321a15f4c5
Georg Ahnert <mail@geahnert.de>
2020-06-22 12:20:28 +0200

    Add intro text

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,89 @@
 <template>
   <div class="max-w-6xl min-h-screen mx-auto flex flex-col justify-center">
     <h1
-      class="self-end text-4xl font-semibold text-gray-700 w-full text-center my-16"
+      class="self-end text-4xl font-semibold text-gray-700 w-full text-center mt-16 mb-8"
     >
-      FairCeptron
+      FAI<span class="tracking-widest">R</span>CEPTRON
     </h1>
-    <div class="text-gray-700 p-4">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor
-      tempor congue consequat. Quisque maximus est enim, eget elementum sapien
-      egestas vitae. Curabitur et est eu sem vestibulum vulputate. Nulla
-      consectetur quam eget orci sollicitudin ornare. Quisque eu venenatis orci.
-      Etiam ut volutpat augue, ac consectetur leo. Vestibulum urna dui,
-      hendrerit sed dolor vel, efficitur semper ipsum. Cras sed aliquet mi.
-      Pellentesque id vestibulum purus. Sed dictum vitae urna eu maximus. Mauris
-      interdum libero eget libero tincidunt vehicula. Mauris lobortis lorem
-      vitae tristique dictum. Vestibulum id maximus velit. Phasellus imperdiet
-      pellentesque erat, vestibulum facilisis enim gravida quis. Integer a quam
-      fermentum, fermentum orci in, venenatis quam. Phasellus augue justo,
-      placerat ut tempor ac, finibus at risus. Vestibulum vitae diam libero. Ut
-      eleifend, elit ut venenatis interdum, est mauris ornare lectus, in
-      consequat leo tortor porttitor erat. Duis pellentesque rutrum arcu, eu
-      mattis lectus interdum nec. Etiam non odio nec libero rutrum bibendum vel
-      et eros. Nullam pharetra bibendum erat, ut rutrum ligula efficitur sit
-      amet. Aenean ac condimentum dui, sit amet vulputate ante. Etiam eros ante,
-      ultrices quis tellus in, laoreet laoreet nunc. Quisque a blandit tortor.
+    <div class="text-gray-700 px-4">
+      <div class="mb-2">
+        Algorithmen übernehmen zunehmend Entscheidungen über Menschen, die
+        bisher von Menschen getroffen wurden. Solche Entscheidungen umfassen
+        unter unter anderem die Vergabe von Krediten, das Gewähren von Kaution
+        oder die Rangfolge möglicher Kandidat:innen für eine offene
+        Arbeitsstelle.
+      </div>
+      <div class="mb-2">
+        Das FAI<span class="tracking-widest">R</span>CEPTRON ist eine Plattform
+        zur Erfassung der menschlichen Perspektive auf solche maschinell
+        getroffenen Auswahl- und Rangfolge-Entscheidungen.
+      </div>
+      <h1 class="text-xl font-semibold mt-6 mb-2">Ablauf und Inhalt</h1>
+      <div class="mb-2">
+        Im Kontext der Vergabe von Studienplätzen werden dir 20 Szenarien mit
+        unterschiedlichen Auswahlen und Rangfolgen gezeigt. Jedes Szenario
+        beinhaltet 10 Personen mit verschiedenen Eigenschaften, die sich alle
+        jeweils um dieselben Studienplätze bewerben. In Auswahlszenarien werden
+        jeweils weniger als 10 Bewerber:innen ausgewählt. In Rangfolge-Szenarien
+        werden alle Bewerber:innen so sortiert, dass die ersten mit der höchsten
+        Wahrscheinlichkeit einen Studienplatz erhalten und die letzten mit der
+        geringsten.
+      </div>
+      <div class="mb-2">
+        Als außenstehender Beobachter bewertest du die verschiedenen
+        algorithmisch getroffenen Entscheidungen nach ihrer Fairness. Dabei
+        stehen dir genau dieselben anonymisierten Informationen über die
+        Bewerber:innen zur Verfügung, wie auch dem Algorithmus.
+      </div>
+      <h1 class="text-xl font-semibold mt-6 mb-2">Teilnahme und Zustimmung</h1>
+      <div class="mb-2">
+        Die Teilnahme an der Studie ist freiwillig. Du kannst sie jederzeit
+        abbrechen. Die Studie nimmt etwa ?? Minuten in Anspruch. Zusätzlich zu
+        den von dir explizit eingegebenen Daten und der Bearbeitungsdauer pro
+        Szenario werden keine weiteren Daten erhoben. Genauere Informationen zum
+        Datenschutz findest du im folgenden Abschnitt. Darüber hinaus wird eine
+        eindeutige Benutzerkennung im Lokalen Speicher deines Webbrowsers
+        hinterlegt (ähnlich zu einem Cookie), welche für die Verwendung des
+        FAI<span class="tracking-widest">R</span>CEPTRONs notwendig ist. Indem
+        du das FAI<span class="tracking-widest">R</span>CEPTRON startest,
+        stimmst du der Verwendung dieser Benutzerkennung zu und bestätigst, dass
+        du
+      </div>
+      <h1
+        class="text-xl font-semibold mt-6 mb-2 cursor-pointer"
+        @click="togglePrivacyStatement"
+      >
+        Datenschutz
+        <span v-if="!showPrivacyStatement" class="font-normal">
+          [Anzeigen]
+        </span>
+        <span v-if="showPrivacyStatement" class="font-normal">
+          [Verbergen]
+        </span>
+      </h1>
+      <div v-if="showPrivacyStatement" class="mb-2">
+        Verantwortlich für die Erhebung, Verarbeitung und Nutzung deiner
+        personenbezogenen Daten Sinne von Art. 4 Nr. 7 DSGVO ist <br />
+        Georg Ahnert <br />
+        Jupp-Müller-Str. 5 <br />
+        52072 Aachen <br />
+        Germany
+      </div>
+      <div v-if="showPrivacyStatement" class="mb-2">
+        Du hast das Recht, auf Antrag unentgeltlich eine Auskunft über die
+        gespeicherten personenbezogenen Daten anzufordern und/oder eine
+        Berichtigung, Sperrung oder Löschung zu verlangen. Eine Sperrung oder
+        Löschung kann nicht erfolgen, wenn gesetzliche Regelungen dem
+        entgegenstehen. Bitte kontaktiere mich unter
+        georg.ahnert&nbsp;[ät]&nbsp;rwth-aachen.de .
+      </div>
+      <div v-if="showPrivacyStatement" class="mb-2">
+        Der Zugriff auf diese Webseite wird grundsätzlich nicht automatisiert
+        erfasst. Es werden keine IP-Adressen erfasst. Deine Eingaben im Verlauf
+        der Studie, sowie die Bearbeitungsdauer pro Frage speichere ich auf
+        meinem Server und stelle sie ggf. anderen Forschern am Lehrstuhl für
+        Computational Social Sciences der RWTH Aachen für zur Verfügung.
+      </div>
     </div>
     <div class="w-full text-center my-16">
       <button
@@ -31,7 +91,7 @@
         class="bg-green-600 hover:bg-green-700 text-white rounded px-4 py-2"
         @click.prevent="startSurvey()"
       >
-        {{ startDE }}
+        FAI<span class="tracking-widest">R</span>CEPTRON starten
       </button>
     </div>
   </div>
@@ -41,7 +101,7 @@
 export default {
   data() {
     return {
-      startDE: 'FairCeptron starten',
+      showPrivacyStatement: false,
     }
   },
   mounted() {
@@ -51,6 +111,9 @@ export default {
     startSurvey() {
       if (!this.$store.getters.userID) this.$store.dispatch('registerUser')
       this.$router.push('/questions/1')
+    },
+    togglePrivacyStatement() {
+      this.showPrivacyStatement = !this.showPrivacyStatement
     },
   },
   transition(to, from) {
