c3ed2de9345bc52c6ff29d5b36a0674360c78a3e
Georg Ahnert <mail@geahnert.de>
2020-07-06 12:06:49 +0200

    Add value indicator on range input

--- components/demographics/Range.vue
+++ components/demographics/Range.vue
@@ -5,17 +5,30 @@
     >
       {{ labelLeft }}
     </div>
-    <input
-      v-model="ratingValue"
-      type="range"
-      :min="ratingMin"
-      :max="ratingMax"
-      :step="ratingStep"
-      :class="ratingSelected ? 'range-visible' : 'range-invisible'"
-      class="mx-4 w-full"
-      @mousedown="enterSelection"
-      @touchstart="enterSelection"
-    />
+    <div class="mx-4 w-full relative">
+      <div
+        v-if="ratingSelected"
+        :style="ratingValueCSS"
+        style="left: var(--rating-value);"
+        class="absolute range-bubble bg-gray-400 rounded-full text-white flex items-center justify-center"
+      >
+        <div class="range-arrow-down absolute"></div>
+        <div class="mb-px text-gray-700 select-none text-xs">
+          {{ Math.round(ratingValue * 100) }}
+        </div>
+      </div>
+      <input
+        v-model="ratingValue"
+        type="range"
+        :min="ratingMin"
+        :max="ratingMax"
+        :step="ratingStep"
+        :class="ratingSelected ? 'range-visible' : 'range-invisible'"
+        class="w-full"
+        @mousedown="enterSelection"
+        @touchstart="enterSelection"
+      />
+    </div>
     <div class="w-32 text-xs sm:text-sm text-gray-700 sm:whitespace-no-wrap">
       {{ labelRight }}
     </div>
@@ -41,8 +54,20 @@ export default {
       ratingValue: undefined,
       ratingMin: 0,
       ratingMax: 1,
-      ratingStep: 0.001,
+      ratingStep: 0.01,
     }
+  },
+  computed: {
+    ratingValueCSS() {
+      return {
+        '--rating-value':
+          'calc(' +
+          this.ratingValue * 100 +
+          '% - ' +
+          (this.ratingValue * 0.5 + 0.5) +
+          'rem)',
+      }
+    },
   },
   watch: {
     ratingValue(val, oldVal) {
